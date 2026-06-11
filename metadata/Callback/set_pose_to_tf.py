345e5454d5ed1c6ed49bbece2716a64bb17db7f1
MithunKinarullathil <51120171+MithunKinarullathil@users.noreply.github.com>
2026-06-11 17:02:12 +0100

    Fix some bugs.

--- metadata/Callback/set_pose_to_tf.py
+++ metadata/Callback/set_pose_to_tf.py
@@ -2,13 +2,15 @@ from geometry_msgs.msg import TransformStamped
 from tf2_ros.static_transform_broadcaster import StaticTransformBroadcaster
 import rospy
 import sys
-import tf.geometry_msgs
+import time
+
+# Wait for a bit?
+time.sleep(5)
 
 stat_br = StaticTransformBroadcaster()
 t = TransformStamped()
-t.header.stamp = rospy.Time.now()
-t.header.frame_id = "odom"
-t.child_frame_id = "map"
+t.header.frame_id = "map"
+t.child_frame_id = "odom"
 
 # Translation
 t.transform.translation = msg.pose.pose.position
@@ -18,7 +20,3 @@ t.transform.rotation = msg.pose.pose.orientation
 
 # Send
 stat_br.sendTransform(t)
-
-# Shutdown
-rospy.signal_shutdown("Finished setting pose. Quitting now.")
-sys.exit()
